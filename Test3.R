72c0bc2d858da49f8d3f8c15816a7b357a166c15
isbool <88773919+isbool@users.noreply.github.com>
2024-06-27 17:41:38 +0300

    Update Test3.R

--- Test3.R
+++ Test3.R
@@ -91,61 +91,61 @@ mytheme <- gridExtra::ttheme_default(
 # Load the tab-delimited file containing the values to be analyzed (samples names in the first column)
 original_table <- read.table (file = input_filename, check.names = FALSE, header = TRUE, dec = ".", sep = "\t", row.names = 1, comment.char = "")
 
-#####################################################################################################################
-####                                      Pre-processing of OTUs Table                                       ########
-#####################################################################################################################
+# #####################################################################################################################
+# ####                                      Pre-processing of OTUs Table                                       ########
+# #####################################################################################################################
 
-# Convert independent variable into factor to avoid errors
-original_table[,independant_variable_name] <- as.factor(original_table[,independant_variable_name]) 
+# # Convert independent variable into factor to avoid errors
+# original_table[,independant_variable_name] <- as.factor(original_table[,independant_variable_name]) 
 
-# Store independent variable columns from original table 
-independent_variable <- original_table[[independant_variable_name]]
-ifelse(group_order != "", independent_variable <- factor(independent_variable,levels=group_order), independent_variable)
+# # Store independent variable columns from original table 
+# independent_variable <- original_table[[independant_variable_name]]
+# ifelse(group_order != "", independent_variable <- factor(independent_variable,levels=group_order), independent_variable)
 
-# Store metadata variable columns from original table 
-my_meta_data <- original_table[1:taxonomic_variables_start - 1]
+# # Store metadata variable columns from original table 
+# my_meta_data <- original_table[1:taxonomic_variables_start - 1]
 
-# Store relative abundance values of all OTUs
-my_otu_data <- original_table[taxonomic_variables_start:dim(original_table)[2]]
+# # Store relative abundance values of all OTUs
+# my_otu_data <- original_table[taxonomic_variables_start:dim(original_table)[2]]
 
-# Transform data by zeroing very low abundances (based on given abundance cutoff - abundance_cutoff)
-my_otu_mod =  as.data.frame(apply(my_otu_data,2,abundance.fix))
+# # Transform data by zeroing very low abundances (based on given abundance cutoff - abundance_cutoff)
+# my_otu_mod =  as.data.frame(apply(my_otu_data,2,abundance.fix))
 
-# Transform data by replacing all zero values with missing values
-# Column consisting of "NA" or "0" only are removed (see below)
-my_otu_mod_noz = as.data.frame(apply(my_otu_mod,2,fill_zero.NA,ReplaceZero))
+# # Transform data by replacing all zero values with missing values
+# # Column consisting of "NA" or "0" only are removed (see below)
+# my_otu_mod_noz = as.data.frame(apply(my_otu_mod,2,fill_zero.NA,ReplaceZero))
 
-# Remove column if entire OTU column contain zeros or missing values
-my_otu_mod_noz <- my_otu_mod_noz[,!apply(my_otu_mod_noz , 2 , function(x) all(is.na(x) | (x == 0))), drop = FALSE]
+# # Remove column if entire OTU column contain zeros or missing values
+# my_otu_mod_noz <- my_otu_mod_noz[,!apply(my_otu_mod_noz , 2 , function(x) all(is.na(x) | (x == 0))), drop = FALSE]
                                                        
-# Transform data by removing any OTU with median relative abundance below cutoff
-t_otu_mod_noz = as.data.frame(t(my_otu_mod_noz))
+# # Transform data by removing any OTU with median relative abundance below cutoff
+# t_otu_mod_noz = as.data.frame(t(my_otu_mod_noz))
 
-# Calculate median for each OTUs
-t_otu_mod_noz$max.median <- apply(t_otu_mod_noz,1,max.med)
+# # Calculate median for each OTUs
+# t_otu_mod_noz$max.median <- apply(t_otu_mod_noz,1,max.med)
 
-# Select OTUs above median cutoff (med.cutoff)
-selected_max <- t_otu_mod_noz[t_otu_mod_noz$max.median > max_median_cutoff,]
+# # Select OTUs above median cutoff (med.cutoff)
+# selected_max <- t_otu_mod_noz[t_otu_mod_noz$max.median > max_median_cutoff,]
 
-# Remove calculated median column "max.median"
-selected_max$max.median <- NULL
+# # Remove calculated median column "max.median"
+# selected_max$max.median <- NULL
 
-# Make a separate object as data frame (columns are OTUs and rows are samples)
-otu_mod_noz_max <- as.data.frame(t(selected_max))
+# # Make a separate object as data frame (columns are OTUs and rows are samples)
+# otu_mod_noz_max <- as.data.frame(t(selected_max))
 
-# Transpose data (columns are samples and rows are OTUs)
-t_otu_mod_noz_max = as.data.frame(t(otu_mod_noz_max))
+# # Transpose data (columns are samples and rows are OTUs)
+# t_otu_mod_noz_max = as.data.frame(t(otu_mod_noz_max))
 
-# Transform data by removing all OTUs with prevalence below the given cutoff
-t_otu_mod_noz_max$pre <- apply(t_otu_mod_noz_max,1,max.pre)
-selected_pre <- t_otu_mod_noz_max[t_otu_mod_noz_max$pre > prevalence_cutoff,]
-selected_pre$pre <- NULL
+# # Transform data by removing all OTUs with prevalence below the given cutoff
+# t_otu_mod_noz_max$pre <- apply(t_otu_mod_noz_max,1,max.pre)
+# selected_pre <- t_otu_mod_noz_max[t_otu_mod_noz_max$pre > prevalence_cutoff,]
+# selected_pre$pre <- NULL
 
-# Transform and filter OTU table
-otu_mod_noz_max_pre <- as.data.frame(t(selected_pre))
+# # Transform and filter OTU table
+# otu_mod_noz_max_pre <- as.data.frame(t(selected_pre))
 
-# Merge the metadata and OTU data in one table 
-# This table will be used as input for the analysis
-input_table <- cbind(my_meta_data,otu_mod_noz_max_pre) 
+# # Merge the metadata and OTU data in one table 
+# # This table will be used as input for the analysis
+# input_table <- cbind(my_meta_data,otu_mod_noz_max_pre) 
 
 
